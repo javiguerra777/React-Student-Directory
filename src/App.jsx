1763854a7277e5a56d638396ff3fa2006efb0e58
Javier Guerra <javier.guerra1001@gmail.com>
2022-04-20 13:59:29 -0700

    adding new students

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import './App.css';
-import people from './services/people.json';
+import Table from './components/Table';
 function App() {
-  return (
-    <>
-    <section className='data'>
-          <header id='headerRow'>
-            <h5>First Name:</h5>
-            <h5>Last Name:</h5>
-            <h5>Email:</h5>
-            <h5>Department:</h5>
-          </header>
-          <main id="body">
-            <code>
-            {people.map(({firstName, lastName, email, department}) => 
-            <>
-            <div id="grid-container">
-              <li className='item'>{firstName.replace(firstName[0], firstName[0].toUpperCase())}</li> 
-              <li className='item'>{lastName.replace(lastName[0], lastName[0].toUpperCase())}</li> 
-              <li className='item'>{email.toLowerCase()}</li> 
-              <li className='item'>{department}</li>
-            </div>
-            </>
-            )
-            }
-            </code>
-          </main> 
-    </section>
-    </>
+  return(
+  
+  <Table />
+  
   );
 }
 
