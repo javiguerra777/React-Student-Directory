6d6aa052e71f4f1e688aabedb56460952bd2fea0
Ermiass <Erohailu@gmail.com>
2022-04-20 17:08:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Form from './components/Form';
+import Search from './components/Search';
 
 function App() {
   return (
     <>
-    <h1>hello</h1>
-    <h1>start coding, main branch, so please make a different branch</h1>
+    {/* <h1>hello</h1> */}
+    {/* <h1>start coding, main branch, so please make a different branch</h1> */}
+    <Form />
+    <Search/>
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,100 @@
+import React,{useState} from 'react'
+import './form.css'
+const Form = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [departement, setDepartement] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    
+    
+
+    
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setDepartement('');
+  };
+
+  
+
+  return (
+    <section >
+      
+      <form onSubmit={handleFormSubmit} >
+        <fieldset>
+        <legend>Student Directory</legend>
+       <div className="formcontainer">
+          <div>
+          <label htmlFor="firstName">
+            Your Name: </label><br></br>
+            <input
+              type="text"
+              id="firstName"
+           
+              placeholder="first name"
+              value={firstName}
+              onChange={(e) =>  setFirstName(e.target.value)}
+            />
+         
+        </div>
+        <div>
+          <label htmlFor="lastName">
+          Your LastName: </label> <br></br>
+            <input
+              type="text"
+              id="lastName"
+              placeholder="last name"
+     
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+         
+        </div>
+        <div>
+          <label htmlFor="email">
+          Your email: </label> <br></br>
+            <input
+              type="email"
+              id="email"
+              placeholder="abc@gdm.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+         
+        </div>
+        <div>
+          <label htmlFor="department" >
+            Departement: </label> <br></br>
+            <select  onChange={(e) => setDepartement(e.target.value)} >
+              {/* // type="text"
+              // id="departement"
+              // placeholder=""
+              // value={departement}
+              */}
+               <option value="department">departement</option>
+            <option value={departement}>Art</option>
+            <option value={departement}>Software engineging</option>
+            <option value={departement}>Communications</option>
+            <option value={departement}>Psycology</option>     
+             
+            
+            </select>
+     
+        </div>
+
+        </div> 
+        <div className="btn">
+        <button  type="submit" required>Add a Student</button>
+        </div>
+       
+        </fieldset>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
